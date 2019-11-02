fe33d5bf4b1b9890dd075c545e596369f8625f2b
Théo <theo_38180@live.fr>
2019-11-02 02:40:25 +0100

    Première version (à peu près, surement) fonctionnelle

---
+++ js/main.js
@@ -0,0 +1,80 @@
+let now = new Date();
+let heure = now.getHours();
+let musique = new Howl({
+    src: [`musiques/normal/${heure}.mp3`],
+    autoplay: true,
+    loop: true,
+    volume: 0.5
+});
+
+const afficheHeure = document.querySelector('#heure');
+
+var Timer = setInterval(myTimer);
+function myTimer(){
+    let date = new Date();
+    afficheHeure.innerHTML = date.toLocaleTimeString();
+    if(date.getHours() != HeureLancement){
+        HeureLancement = date.getHours();
+        musique.fade(1,0, 2000);
+        setTimeout(musiquechange1, 2000); 
+        function musiquechange1(){
+        musique.pause();
+        musique.unload();
+        musique._src = `musiques/normal/${HeureLancement}.mp3`;
+        musique.load();
+        if(musique.playing() == true){
+            musique.play();
+            setTimeout(musiquechange2, 1000); 
+            function musiquechange2(){
+            musique.fade(0,1, 1000);
+            }
+            }
+        }
+    }
+}
+
+let lancement = true;
+let HeureLancement = heure;
+
+const player = document.querySelector('i');
+player.addEventListener("click", function(){
+    if(player.innerHTML === "play_circle_outline"){
+        player.innerHTML = "pause_circle_outline";
+        if(lancement == true){
+            musique.play();
+            lancement = false;
+        }
+        else{
+            musique.play();
+            setTimeout(mafonction, 300); 
+            function mafonction(){
+            musique.fade(0,1, 300);
+            }
+        } 
+    }
+    else{
+        player.innerHTML = "play_circle_outline";
+        musique.fade(1,0, 300);
+        setTimeout(mafonction2, 300); 
+        function mafonction2(){
+            musique.pause();
+        }
+    }
+})
+
+const slider = document.getElementById("myRange");
+Howler.volume(slider.value/100);
+
+slider.oninput = function() {
+    Howler.volume(slider.value/100);
+    if(slider.value/100 == 0){
+        player.innerHTML = "play_circle_outline";
+        musique.pause();
+    }
+    else{
+        if(musique.playing() == false){
+            player.innerHTML = "pause_circle_outline";
+            musique.play();
+        }
+    }
+}
