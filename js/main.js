ac4733c72acd6573bc72828eb4aaf8129579b6c3
Théo <theo_38180@live.fr>
2019-12-18 11:24:40 +0100

    De la neige à l'orage
    Ajout des thèmes pour la neige à l'orage !

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var valeurMeteoOK = "unset";
 let firstPage = false;
 let changementHeure = false;
 let SplashScreen = true;
-//setTimeout(clear, 1);
+setTimeout(clear, 1);
 
 function clear(){
     window.console.clear();
@@ -26,14 +26,18 @@ const cityPosInput = document.querySelector('.cityPos form input');
 const cityPosFormError = document.querySelector('.cityPosFormError');
 
 
-if(localStorage.getItem("ville") == ""){
+if(localStorage.getItem("ville") == "" || localStorage.getItem("ville") == undefined){
     PageOk.style.opacity = 0;
     FirstPage.style.opacity = 1;
     SplashScreen = true;
+    PageOk.style.zIndex = -100000;
+    FirstPage.style.zIndex = 100000;
 }
 else{
     PageOk.style.opacity = 1;
+    PageOk.style.zIndex = 100000;
     FirstPage.style.opacity = 0;
+    FirstPage.style.zIndex = -100000;
     searchWeather(localStorage.getItem("ville"));
     SplashScreen = false;
 }
@@ -71,6 +75,8 @@ function resetVille(){
     localStorage.setItem("ville", "");
     PageOk.style.opacity = 0;
     FirstPage.style.opacity = 1;
+    PageOk.style.zIndex = -100000;
+    FirstPage.style.zIndex = 100000;
     firstPage = false;
     SplashScreen = true;
     document.querySelector('input').value = "";
@@ -107,6 +113,38 @@ function changeLocation(){
 function changeMeteoBRUTE(meteo){
     valeurMeteoTEMP = meteo;
     changebg(verifHeureBG());
+    valeurMeteoOK = "Custom";
+
+    if(valeurMeteoTEMP == "Pluie" || valeurMeteoTEMP == "Orage"){
+        valeurMeteoMUSIQUE = "Pluie";
+    }
+    else if(valeurMeteoTEMP == "Neige"){
+        valeurMeteoMUSIQUE = "Neige";
+    }
+    else{
+        valeurMeteoMUSIQUE = "Normal";
+    }
+
+    let isplaying = true;
+    if(musique.playing() == false){
+        isplaying = false;
+    }
+
+    musique.fade(slider.value/100, 0, 2000);
+    setTimeout(musiquechange1, 2000); 
+    function musiquechange1(){
+        musique.pause();
+        musique.unload();
+        musique._src = `musiques/${valeurMeteoMUSIQUE}/${HeureLancement}.mp3`;
+        musique.load();
+        if(isplaying == true){
+            musique.play();
+        }
+        setTimeout(musiquechange2, 1000); 
+        function musiquechange2(){
+            musique.fade(0, slider.value/100, 1000);
+        } 
+    }
 }
 
 //Coeur du programme, permet de chercher la météo et change la musique, l'arière plan et la position du soleil
@@ -130,6 +168,8 @@ function searchWeather(ville){
             console.log(localStorage.getItem("ville"));
             PageOk.style.opacity = 1;
             FirstPage.style.opacity = 0;
+            PageOk.style.zIndex = 100000;
+            FirstPage.style.zIndex = -100000;
 
             if(valeurMeteoOK == result.weather[0].main && !changementHeure){
                 console.log("La météo n'a pas changé");
@@ -137,9 +177,14 @@ function searchWeather(ville){
             else{
                 changementHeure = false;
                 valeurMeteoOK = result.weather[0].main;
+                //valeurMeteoOK = "Snow";
                 console.log(`Il fait ${valeurMeteoOK} à ${result.name}`);
                 if(valeurMeteoOK == "Rain"){
                     valeurMeteoTEMP = "Pluie";
+                    valeurMeteoMUSIQUE = "Pluie";
+                }
+                else if(valeurMeteoOK == "Thunderstorm"){
+                    valeurMeteoTEMP = "Orage";
                     valeurMeteoMUSIQUE = "Pluie";
                 }
                 else if(valeurMeteoOK == "Clear"){
@@ -149,6 +194,10 @@ function searchWeather(ville){
                 else if(valeurMeteoOK == "Mist" || valeurMeteoOK == "Fog"){
                     valeurMeteoTEMP = "Brouillard";
                     valeurMeteoMUSIQUE = "Normal";
+                }
+                else if(valeurMeteoOK == "Snow"){
+                    valeurMeteoTEMP = "Neige";
+                    valeurMeteoMUSIQUE = "Neige";
                 }
                 else{
                     valeurMeteoTEMP = "Normal";
@@ -215,22 +264,47 @@ let makeItRain = function() {
 
     };
 
-    let pluieOk = false;
+let pluieOk = false;
 
-    function rainOpacity(){
+function rainOpacity(){
         pluie.classList = "rain rainVisible front-row"
+}
+
+function lancerPluie(){
+    if(pluieOk == false){
+        pluie.classList = "rain rainInvisible front-row"
+        makeItRain();
+        setTimeout(rainOpacity, 650);
+
+        pluieOk = true;
     }
+}
 
-    function lancerPluie(){
-        if(pluieOk == false){
-            pluie.classList = "rain rainInvisible front-row"
-            makeItRain();
-            setTimeout(rainOpacity, 650);
+let musiqueEclair = new Howl({
+    src: [`musiques/Eclair/1.mp3`],
+    autoplay: false,
+    loop: false,
+    volume: 0.2
+});
 
-            pluieOk = true;
-        }
+function EclairSound(){
+    if(musique.playing() == true){
+        musiqueEclair.unload();
+        musiqueEclair._src = `musiques/Eclair/${Math.floor((3)*Math.random()+1)}.mp3`;
+        musiqueEclair.load();
+        musiqueEclair.play();
     }
+    setTimeout(changePosLightling, 2000);
+} 
 
+function activerLightning(){
+    setInterval(EclairSound, 7000);
+}
+
+function changePosLightling(){
+    let PosAlea = Math.floor((40)*Math.random()-20);
+    Lightning.style.marginLeft = `${PosAlea}vw`;
+}
 /*Selecteurs*/
 
 const LocationButton = document.querySelector("#locationButton");
@@ -242,12 +316,14 @@ const Mouvement = document.querySelector(".astre");
 const NuageImage1 = document.querySelector('.nuage1');
 const NuageImage2 = document.querySelector('.nuage2');
 const BgRain = document.querySelector('#bgRainSelector');
+const Lightning = document.querySelector('.lightning');
 const RainSelector = document.querySelector('.front-row');
 const BgEtoiles = document.querySelector('.bgStars1');
 const TextColor = document.querySelector('#heure');
 const PlayerColor = document.querySelector('#playpause');
 const SliderColor = document.querySelector('#myRange');
 const VolumeIcon = document.querySelectorAll('#volume');
+const NeigeFalling = document.querySelector('#snow');
 
 let lancement = true;
 let HeureLancement = heure;
@@ -317,11 +393,34 @@ function changebg(valeurBG){
     document.querySelectorAll('.cityPos button')[0].className = `cityPosButtonColor${valeurMeteoTEMP}${valeurBG}`;
     document.querySelectorAll('.cityPos button')[1].className = `cityPosButtonColor${valeurMeteoTEMP}${valeurBG}`;
     Mouvement.style.opacity = 1;
+    Lightning.style.opacity = 0;
+    Lightning.style.animationName = "none";
+    musiqueEclair.volume(0);
+    NeigeFalling.style.opacity = 0;
+    BgInColor.style.backgroundImage = 'url("medias/Plaine\ flat2.png")';
 
     if(valeurMeteoTEMP == "Pluie"){
         BgRain.className = `bgRain${valeurBG}`;
         RainSelector.className = 'rain rainVisible front-row';
         lancerPluie();
+        Mouvement.style.opacity = 0.2;
+    }
+    else if(valeurMeteoTEMP == "Orage"){
+        BgRain.className = `bgRainOrage${valeurBG}`;
+        RainSelector.className = 'rain rainVisible front-row';
+        lancerPluie();
+        Mouvement.style.opacity = 0.2;
+        Lightning.style.opacity = 0;
+        Lightning.style.animationName = "eclair";
+        setTimeout(activerLightning, 1500);
+        musiqueEclair.volume(0.2);
+    }
+    else if(valeurMeteoTEMP == "Neige"){
+        BgInColor.style.backgroundImage = 'url("medias/Plaine\ flatNeige.png")';
+        BgRain.className = `bgRainNeige${valeurBG}`;
+        RainSelector.className = 'rain rainInvisible front-row';
+        Mouvement.style.opacity = 0.5;
+        NeigeFalling.style.opacity = 1;
     }
     else if(valeurMeteoTEMP == "Brouillard"){
         Mouvement.style.opacity = 0.2;
